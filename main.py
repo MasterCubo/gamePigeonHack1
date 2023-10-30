5e3a9d01be559db6ef5383fd8bc50dbb75941dbd
MasterCubo-DESKTOP <mastercubo.osu@gmail.com>
2023-10-29 23:18:12 -0400

    initial commit try 2

---
+++ main.py
@@ -0,0 +1,100 @@
+import pyautogui as pag
+import time
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw
+from math import sqrt, pi, cos, sin
+from circleDetector import canny_edge_detector
+from collections import defaultdict
+
+# pag = x=-1192, y=401
+# paint = 728, 39
+
+# top left pag = x=-1920, y=363
+# bottom right pag= -1 1442
+# paint  0,0   1920,1080
+
+#while True:
+#    pagX, pagY = pag.position()
+#    paintX=pagX+1920
+#    paintY=pagY-363
+#    print(f'x={paintX}, y={paintY}')
+#    time.sleep(0.1)
+
+#while True:
+#    print(pag.position())
+#    time.sleep(0.1)
+
+startTime = time.time()
+# im = pag.screenshot(region=(728-1920, 39+363, 1191-728,1053-39))
+im = pag.screenshot(region=(1084,374,1464-1084,1124-374))
+im.save(r"firstInput.png")
+imcv = np.array(im)[:, :, ::-1]
+print(f'screenshot taken in {time.time()-startTime} seconds')
+
+# Dark Felt Color = 2,78,70
+# Light Felt Color = 22,149,128
+img = Image.open(r"firstInput.png")
+pixdata = img.load()
+for y in range(img.size[1]):
+    for x in range(img.size[0]):
+         r, g, b = img.getpixel((x, y))
+         if (0<r<55) and (70<g<170) and (60<b<150):
+              pixdata[x, y] = (255, 105, 180)
+img.save('chromaInput.png')
+print(f'chroma keyed in {time.time()-startTime} seconds')
+
+
+
+
+input_image=Image.open(r"chromaInput.png")
+output_image = Image.new("RGB", input_image.size)
+output_image.paste(input_image)
+draw_result = ImageDraw.Draw(output_image)
+# Find circles
+rmin = 10
+rmax = 13
+steps = 200
+threshold = 0.4
+
+
+points = []
+for r in range(rmin, rmax + 1):
+    for t in range(steps):
+        points.append((r, int(r * cos(2 * pi * t / steps)), int(r * sin(2 * pi * t / steps))))
+
+print(f'points appended in {time.time()-startTime} seconds')
+
+# Function to check if two circles intersect at all
+def do_circles_intersect(x1, y1, r1, x2, y2, r2):
+    distance_between_centers = sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    return distance_between_centers < r1 + r2
+
+acc = defaultdict(int)
+for x, y in canny_edge_detector(input_image):
+    for r, dx, dy in points:
+        a = x - dx
+        b = y - dy
+        acc[(a, b, r)] += 1
+
+circles = []
+circle_centers = []
+for k, v in sorted(acc.items(), key=lambda i: -i[1]):
+    x, y, r = k
+    if v / steps >= threshold and not any(do_circles_intersect(x,y,r,xc,yc, rc) for xc, yc, rc in circles):
+        #print(v / steps, x, y, r)
+        circles.append((x, y, r))
+        circle_centers.append((x,y))
+print(f'circles found in {time.time()-startTime} seconds')
+
+for x, y, r in circles:
+    r=10 # override since we know radius
+    draw_result.ellipse((x-r, y-r, x+r, y+r), outline=(0,0,255,0))
+print(f'cirlces drawn in {time.time()-startTime} seconds')
+
+# Save output image
+output_image.save("result.png")
+
+# alternative methods for better calculations down the road. Take the centers of the balls, and paste over the pixels from the original screenshot within a set radius, since we KNOW that all the balls have the same radius.
+# do a whole second pass on the circle detection by re-chroma-ing usiong the first circle pass to un chroma an area around each ball. ?
+# when drawing the circles, fill in with a certain color, making the stripes different if we can.
---
+++ paintToPAG.py
@@ -0,0 +1,11 @@
+# pag = x=-1192, y=401
+# paint = 728, 39
+
+# top left pag = x=-1920, y=363
+# bottom right pag= -1 1442
+# paint  0,0   1920,1080
+
+paintX=None
+paintY=None
+pagX=paintX-1920
+pagY=paintY-362
